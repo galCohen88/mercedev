6cfe5dc5c047e05cbe90d9179be11806a66b0227
Gal Cohen <gal.cohen@cloudlock.com>
2017-05-03 17:50:38 +0300

    remove redundant installation script

--- installation_try.sh
+++ installation_try.sh
@@ -1,9 +1,29 @@
-echo "Checking for Terraform installed"
+echo "Creating env dirs"
+mkdir $USER
 
-terraform --version | grep ${TERRAFORM_VERSION}
+echo "Checking for Terraform installed"
+brew ls --versions terraform > /dev/null;
 if [[ $? != 0 ]] ; then
-    # Install terraform
+    echo "Installing Terraform"
     brew install terraform
-    brew switch terraform ${TERRAFORM_VERSION}
+else
+    echo "Terraform already installed"
 fi
 
+echo "Checking for kops installed"
+brew ls --versions kops > /dev/null;
+if [[ $? != 0 ]] ; then
+    echo "Installing kops"
+    brew install kops
+else
+    echo "Kops already installed"
+fi
+
+echo "Checking for kubectl installed"
+brew ls --versions kubectl > /dev/null;
+if [[ $? != 0 ]] ; then
+    echo "Installing kubectl"
+    brew install kubectl
+else
+    echo "Kops already installed"
+fi

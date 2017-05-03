80e1327d1b411023badf8bf4e911a0938dbfa3aa
Gal Cohen <gal.cohen@cloudlock.com>
2017-05-03 17:56:15 +0300

    installation script

--- installation.sh
+++ installation.sh
@@ -1,7 +1,33 @@
-brew update
-terraform --version
-brew install terraform
-brew install kops
-brew install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
+echo "Creating env dirs"
+mkdir $USER
+
+echo "Checking for Terraform installed"
+brew ls --versions terraform > /dev/null;
+if [[ $? != 0 ]] ; then
+    echo "Installing Terraform"
+    brew install terraform
+else
+    echo "Terraform already installed"
+fi
+
+echo "Checking for kops installed"
+brew ls --versions kops > /dev/null;
+if [[ $? != 0 ]] ; then
+    echo "Installing kops"
+    brew install kops
+else
+    echo "Kops already installed"
+fi
+
+echo "Checking for kubectl installed"
+brew ls --versions kubectl > /dev/null;
+if [[ $? != 0 ]] ; then
+    echo "Installing kubectl"
+    brew install kubectl
+else
+    echo "kubectl already installed"
+fi
+
+echo "Creating remote dashboard"
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard-no-rbac.yaml
 
